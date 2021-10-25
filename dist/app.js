f3ad953a9180321d96a477886ca13f1468932914
Nikan Zeyaei <72458440+NikanZeyaei@users.noreply.github.com>
2021-10-25 18:29:26 +0330

    Delete dist directory

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server/server");
-const bot_1 = require("./bot/bot");
-(0, server_1.server)();
-(0, bot_1.botStart)();
